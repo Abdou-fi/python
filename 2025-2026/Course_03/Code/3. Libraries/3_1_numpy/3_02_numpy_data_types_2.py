0055614d2e8fed76f6bb96e4cdbe31a41a5bfcad
Abdesselam Filali <filalia@gmail.com>
2026-01-06 23:33:29 +0100

    __

--- 2025-2026/Course_03/Code/3. Libraries/3_1_numpy/3_02_numpy_data_types_2.py
+++ 2025-2026/Course_03/Code/3. Libraries/3_1_numpy/3_02_numpy_data_types_2.py
@@ -9,12 +9,10 @@ complex_array = np.array([1+2j, 3+4j, 5+6j, 7+8j], dtype=np.complex128)
 print("Integer Array:", int_array)
 print("Float Array:", float_array)
 print("Complex Array:", complex_array)
-
 # Output:
 # Integer Array: [1 2 3 4]
 # Float Array: [1. 2. 3. 4.]
 # Complex Array: [(1.+2.j) (3.+4.j) (5.+6.j) (7.+8.j)]
-
 
 # Checking the data types of the arrays
 print("Data type of int_array:", int_array.dtype)
@@ -25,11 +23,14 @@ print("Data type of complex_array:", complex_array.dtype)
 int_array = np.array([1, 2, 3, 4])
 float_array = int_array.astype(np.float64)
 complex_array = float_array.astype(np.complex128)
+# Output:
+# Data type of int_array: int32
+# Data type of float_array: float64 
+# Data type of complex_array: complex128
 
 # Converted arrays
 print("Converted Float Array:", float_array)
 print("Converted Complex Array:", complex_array)
-
 # Output:
 # Converted Float Array: [1. 2. 3. 4.]
 # Converted Complex Array: [(1.+0.j) (2.+0.j) (3.+0.j) (4.+0.j)]
@@ -37,7 +38,6 @@ print("Converted Complex Array:", complex_array)
 # Checking the data types of the converted arrays
 print("Data type of float_array after conversion:", float_array.dtype)
 print("Data type of complex_array after conversion:", complex_array.dtype)
-
 # Output:
 # Data type of float_array after conversion: float64
 # Data type of complex_array after conversion: complex128
@@ -45,30 +45,31 @@ print("Data type of complex_array after conversion:", complex_array.dtype)
 # Demonstrating type casting during array operations
 arr1 = np.array([1, 2, 3], dtype=np.int32)
 arr2 = np.array([4, 5, 6], dtype=np.int32)
+# Output:
+# Data type of result: int32
 
 # Performing operations and observing type casting
 result = arr1 + arr2
 print("Result of arr1 + arr2:", result)
 print("Data type of result:", result.dtype)
-
 # Output:
 # Result of arr1 + arr2: [5 7 9]
 # Data type of result: int32    
+
 # When adding an integer array to a float array
 arr3 = np.array([1.5, 2.5, 3.5], dtype=np.float64)
 result2 = arr1 + arr3
 print("Result of arr1 + arr3:", result2)
 print("Data type of result2:", result2.dtype)
-
 # Output:   
 # Result of arr1 + arr3: [2.5 4.5 6.5]
 # Data type of result2: float64
+
 # When multiplying an integer array with a complex array
 arr4 = np.array([1+2j, 3+4j, 5+6j], dtype=np.complex128)
 result3 = arr1 * arr4
 print("Result of arr1 * arr4:", result3)
 print("Data type of result3:", result3.dtype)
-
 # Output:   
 # Result of arr1 * arr4: [(1.+2.j) (6.+8.j) (15.+18.j)]
 # Data type of result3: complex128  
@@ -79,16 +80,15 @@ arr6 = np.array([1.5, 2.5, 3.5], dtype=np.float64)
 result4 = arr5 + arr6
 print("Result of arr5 + arr6:", result4)
 print("Data type of result4:", result4.dtype)
-
 # Output:
 # Result of arr5 + arr6: [2.5 4.5 6.5]
 # Data type of result4: float64
+
 # When multiplying an integer array with a complex array
 arr7 = np.array([1+2j, 3+4j, 5+6j], dtype=np.complex128)
 result5 = arr5 * arr7
 print("Result of arr5 * arr7:", result5)
 print("Data type of result5:", result5.dtype)
-
 # Output:
 # Result of arr5 * arr7: [(1.+2.j) (6.+8.j) (15.+18.j)]
 # Data type of result5: complex128
@@ -102,15 +102,16 @@ print("Data type of result6:", result6.dtype)
 # Output:
 # Result of arr8 + arr9: [2.5 4.5 6.5]
 # Data type of result6: float64
+
 # When multiplying an integer array with a complex array
 arr10 = np.array([1+2j, 3+4j, 5+6j], dtype=np.complex128)
 result7 = arr8 * arr10
 print("Result of arr8 * arr10:", result7)
 print("Data type of result7:", result7.dtype)
-
 # Output:
 # Result of arr8 * arr10: [(1.+2.j) (6.+8.j) (15.+18.j)]
 # Data type of result7: complex128
+
 # Demonstrating type casting during array operations with different data types
 arr11 = np.array([1, 2, 3], dtype=np.int32)
 arr12 = np.array([1.5, 2.5, 3.5], dtype=np.float64)
@@ -120,13 +121,12 @@ print("Data type of result8:", result8.dtype)
 # Output:
 # Result of arr11 + arr12: [2.5 4.5 6.5]
 # Data type of result8: float64
+
 # When multiplying an integer array with a complex array
 arr13 = np.array([1+2j, 3+4j, 5+6j], dtype=np.complex128)
 result9 = arr11 * arr13
 print("Result of arr11 * arr13:", result9)
 print("Data type of result9:", result9.dtype)
-
-
 # Output:
 # Result of arr11 * arr13: [(1.+2.j) (6.+8.j) (15.+18.j)]
 # Data type of result9: complex128
@@ -146,10 +146,10 @@ arr16 = np.array([1+2j, 3+4j, 5+6j], dtype=np.complex128)
 result11 = arr14 * arr16
 print("Result of arr14 * arr16:", result11)
 print("Data type of result11:", result11.dtype)
-
 # Output:
 # Result of arr14 * arr16: [(1.+2.j) (6.+8.j) (15.+18.j)]
 # Data type of result11: complex128
+
 # Demonstrating type casting during array operations with different data types
 arr17 = np.array([1, 2, 3], dtype=np.int32)
 arr18 = np.array([1.5, 2.5, 3.5], dtype=np.float64)
