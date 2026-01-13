daa29dac6180ffbe9cb34ef3a7c51843fa1e01f9
Abdesselam Filali <filalia@gmail.com>
2026-01-13 19:42:11 +0100

    __

--- 2025-2026/Course_03/Code/14. fixed point decimals/fixed_point.py
+++ 2025-2026/Course_03/Code/14. fixed point decimals/fixed_point.py
@@ -1,13 +1,13 @@
 from decimal import Decimal, getcontext, localcontext, ROUND_DOWN
 
-# TWOPLACES = Decimal('0.01') # Defines the fixed-point format (two decimal places)
+TWOPLACES = Decimal('0.01') # Defines the fixed-point format (two decimal places)
 
 number = 0.287
 
 with localcontext() as ctx:
     ctx.rounding = ROUND_DOWN
-    d = Decimal(number).quantize(Decimal('0.01'))
-    x= Decimal('2.3399').quantize(d)
-    print(x)  # Outputs: 2.99
+    d1 = Decimal(number).quantize(TWOPLACES)
+    d2 = Decimal(str(number*100)).quantize(TWOPLACES)
+    print(d1)  # Outputs: 2.99
     
-print(x * 1000)
+print(d1 * 1000, d2)
